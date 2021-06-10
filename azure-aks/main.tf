36165bae8a4f0d2d514b1a4c3b8c396999794e93
Mohamed Belgaied Hassine <60746935+belgaied2@users.noreply.github.com>
2021-06-10 10:19:18 +0200

    update name of resource group with only dashes

--- azure-aks/main.tf
+++ azure-aks/main.tf
@@ -1,5 +1,5 @@
 # Create a resource group
 resource "azurerm_resource_group" "rancher_aks" {
-  name     = "${var.name_prefix}-rancher_aks"
+  name     = "${var.name_prefix}-rancher-aks"
   location = var.azure_location
 }
