c6c3348ec46d9446151240a976641bd59f898ffe
Roman Simecek <raskhadafi@users.noreply.github.com>
2021-08-10 12:23:17 +0200

    Add output of the k8s credentials.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "rancher_endpoint" {
   description = "Rancher's HTTPS Endpoint"
   value = "https://rancher.${var.dns_zone_name}"
 }
+
+output "aks_credentials" {
+  description = "Credentials for aks k8s"
+  value =  object({
+    client_key = module.aks.client_key
+    client_certificate = module.aks.client_certificate
+    cluster_ca_certificate = module.aks.cluster_ca_certificate
+    host = module.aks.host
+ })
+}
